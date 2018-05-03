99aa6e147936ea2a4b576553127a264e60bbec36
Jake Overall <joverall22@gmail.com>
2018-05-02 20:05:30 -0600

    Added menu and menuoptions

--- ConsoleLibrary/Models/Library.cs
+++ ConsoleLibrary/Models/Library.cs
@@ -9,20 +9,107 @@ namespace ConsoleLibrary.Models
     public class Library
     {
 
-        string Name { get; set; }
+        public string Name { get; private set; }
         string Address { get; set; }
 
+        public bool Visiting { get; private set; }
+        public bool InSection { get; private set; }
         private List<Book> Books = new List<Book>();
         private List<VideoGame> VideoGames = new List<VideoGame>();
         private List<ILibraryItem> Items = new List<ILibraryItem>();
         private List<ICheckOutable> ItemsForCheckout = new List<ICheckOutable>();
-
+        private Menu MainMenu { get; set; }
+        private Menu BookMenu { get; set; }
 
         public Library(string name, string address)
         {
             Name = name;
             Address = address;
+            Visiting = true;
+            InSection = false;
+            MainMenu = BuildMainMenu();
         }
+
+        Menu BuildMainMenu()
+        {
+            return new Menu(
+                "Main Menu",
+                new List<MenuOption>
+                {
+                    new MenuOption(BookSelection, "Visit the Book Section"),
+                    new MenuOption(VideoGameSelection, "Visit the Video Game Section"),
+                    new MenuOption(LeaveLibrary, "Leave the Library")
+                });
+        }
+
+        public void Setup()
+        {
+            BuildBookMenu();
+        }
+
+        void BuildBookMenu()
+        {
+             BookMenu = new Menu(
+                "Book Menu",
+                new List<MenuOption>
+                {
+                    new MenuOption(PrintBooks, "View all the Books"),
+                    new MenuOption(LeaveSection, "Leave the Section")
+                });
+        }
+
+
+        public void MainMenuSelection()
+        {
+            Action action = MainMenu.SelectOption();
+            if( action != null)
+            {
+                action.Invoke();
+            }
+        }
+
+        private void BookSelection()
+        {
+            Console.Clear();
+            InSection = true;
+            while(InSection)
+            {
+                Action action = BookMenu.SelectOption();
+                if (action != null)
+                {
+                    action.Invoke();
+                }
+            }
+        }
+
+        private void VideoGameSelection()
+        {
+            Console.WriteLine("Under Construction");
+        }
+
+        private void LeaveLibrary()
+        {
+            Console.WriteLine("Thanks for stopping in!");
+            Visiting = false;
+        }
+
+        private void LeaveSection()
+        {
+            Console.WriteLine("You've left the Section!");
+            InSection = false;
+        }
+
+        private void PrintBooks()
+        {
+            Console.Clear();
+            int count = 1;
+            foreach(Book book in Books)
+            {
+                Console.WriteLine($"{count++} {book.Name} - {book.Author}");
+            }
+            Console.ReadLine();
+        }
+
 
         internal void AddItem(ILibraryItem item)
         {
--- ConsoleLibrary/Program.cs
+++ ConsoleLibrary/Program.cs
@@ -7,10 +7,12 @@ namespace ConsoleLibrary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             Library boiseLibrary = new Library("Boise Library", "123 fak str");
+            boiseLibrary.Setup();
 
             Book mobyDick = new Book("Moby Dick", "das90fudas", "Herman Wells");
+            Book awesome = new Book("D$'s Awesome Guide to D&D", "39487237", "D Money");
+            Book readyplayerone = new Book("Ready Player One", "4435ihhf", "Jake Overall");
             var battlefield1942 = new VideoGame() {
                 Name = "Battlefield 1942",
                 Platform = "PC"
@@ -19,8 +21,18 @@ namespace ConsoleLibrary
             var dnd121 = new Magazine("DnD", 121);
 
             boiseLibrary.AddItem(mobyDick);
+            boiseLibrary.AddItem(awesome);
+            boiseLibrary.AddItem(readyplayerone);
             boiseLibrary.AddItem(battlefield1942);
             boiseLibrary.AddItem(dnd121);
+
+            while (boiseLibrary.Visiting)
+            {
+                boiseLibrary.MainMenuSelection();
+            }
+
+
+
 
         }
     }
