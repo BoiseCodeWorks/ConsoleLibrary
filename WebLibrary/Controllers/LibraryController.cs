cfc14e828bc56a814126c945a16c94566dddedf1
Jake Overall <joverall22@gmail.com>
2018-05-10 18:40:37 -0600

    web api stuff

---
+++ WebLibrary/Controllers/LibraryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLibrary.Providers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    public class LibraryController : Controller
+    {
+        private readonly LibraryProvider _lp;
+        // GET api/library
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _lp.GetLibraries();
+        }
+
+
+        public LibraryController()
+        {
+            _lp = new LibraryProvider();
+        }
+
+
+    }
+}
